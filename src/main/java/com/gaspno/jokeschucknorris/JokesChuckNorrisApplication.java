41080c7627a19dd364d943ee2098d470a773dde3
gaspno <zatchgabriel@gmail.com>
2022-09-22 14:46:44 -0300

    Initial commit

---
+++ src/main/java/com/gaspno/jokeschucknorris/JokesChuckNorrisApplication.java
@@ -0,0 +1,13 @@
+package com.gaspno.jokeschucknorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesChuckNorrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesChuckNorrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaspno/jokeschucknorris/JokesChuckNorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspno.jokeschucknorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesChuckNorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
